5ddf89c7786e75526e04776347ba9823a13980f2
girish pote <potegirish6@gmail.com>
2025-08-21 21:19:51 +0530

    First Jenkins Pipeline Project Java

---
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Jenkins CI/CD with Docker Agent!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
